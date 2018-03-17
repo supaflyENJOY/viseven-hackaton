f43dc24f76491afdb490bcb5686fead62d892705
Arsen Petrosyan <supaflyENJOY@users.noreply.github.com>
2018-03-17 06:13:08 +0200

    Frontend: Conversion to one-page components

--- frontend/src/Components/MainPart/index.js
+++ frontend/src/Components/MainPart/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './index.css'
-import ExerciseList from '../ExerciseList';
 import exercise from '../api/exercise';
 import Navbar from '../Navbar';
 
@@ -9,7 +8,31 @@ class MainPart extends Component {
         return (<div>
             <Navbar/>
             <div className='exerciseListBox'>
-                <ExerciseList exercise ={exercise}/>
+                <ul>
+                    {exercise.map(exercise=>
+                        <li key={exercise.ID}>
+
+
+                            <div>
+                                {<section id={exercise.ID}>
+                                    <img className='imageExercice' />
+                                    <a className='titleBox'> {exercise.Title}</a>
+                                    <a className='addExercise' href='#'><img src={require('../img/plus.png')}/></a>
+                                    <a className='showDetails' href='#' ><img src={require('../img/menu-down.png')}/></a>
+                                </section>}
+                                <div className='details'>
+                                    <p>{exercise.Description}</p>
+                                    <ul className='musclesList'>
+                                        {exercise.Muscles.map(muscle =>
+                                            <li id={muscle.ID}>{muscle.Name}</li>
+                                        )}
+                                    </ul>
+                                </div>
+                            </div>
+
+                        </li>
+                    )}
+                </ul>
             </div>
             </div>
             );
