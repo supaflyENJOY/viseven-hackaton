c48469f0999943cc4256d1aeccc9a6b5e8e62e55
Gastonovich <oleksndrkorn@gmail.com>
2018-03-17 11:23:05 +0300

    frontend
    xz

--- frontend/src/Components/MainPart/index.js
+++ frontend/src/Components/MainPart/index.js
@@ -130,9 +130,6 @@ class MainPart extends Component {
                         )}</ul>
                     )}
                     </ul>
-
-
-
             </div>
             </div>
             );
